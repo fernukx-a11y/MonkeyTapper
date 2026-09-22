603f13c0d27cf59d1f4d53a6b992c3bea8b0874b
fernukx-a11y <fernukx@gmail.com>
2026-09-22 03:11:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let coins = 0;
 
+const button = document.getElementById("tap-btn");
+const coinsText = document.querySelector("p");
+
+if (window.Telegram && window.Telegram.WebApp) {
+    window.Telegram.WebApp.ready();
+    window.Telegram.WebApp.expand();
+}
+
+button.addEventListener("click", function () {
+    coins += 1;
+    coinsText.textContent = "💰 Монеты: " + coins;
+});
